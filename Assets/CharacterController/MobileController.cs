1690460520d43d3d3b7533e70f43ac1bc60db8f1
DanielePallavicini <pallavicini.dp@gmail.com>
2021-06-27 23:13:47 +0200

    Blockout update and CharacterController wip

---
+++ Assets/CharacterController/MobileController.cs
@@ -0,0 +1,132 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/MobileController.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @MobileController : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @MobileController()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""MobileController"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerMain"",
+            ""id"": ""1df265dd-525e-4648-a3cd-47079c4e1551"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""5d7a9626-7f7b-4823-809c-ec3d5f5246ec"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""85cc8939-21e5-46e5-9a23-edbbe685e28e"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // PlayerMain
+        m_PlayerMain = asset.FindActionMap("PlayerMain", throwIfNotFound: true);
+        m_PlayerMain_Move = m_PlayerMain.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerMain
+    private readonly InputActionMap m_PlayerMain;
+    private IPlayerMainActions m_PlayerMainActionsCallbackInterface;
+    private readonly InputAction m_PlayerMain_Move;
+    public struct PlayerMainActions
+    {
+        private @MobileController m_Wrapper;
+        public PlayerMainActions(@MobileController wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_PlayerMain_Move;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerMain; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerMainActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerMainActions instance)
+        {
+            if (m_Wrapper.m_PlayerMainActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_PlayerMainActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public PlayerMainActions @PlayerMain => new PlayerMainActions(this);
+    public interface IPlayerMainActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/CharacterController/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private MobileController PlayerInput;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    public float playerSpeed = 2.0f;
+    public float jumpHeight = 1.0f;
+    public float gravityValue = -9.81f;
+
+    public void Awake()
+	{
+		PlayerInput = new MobileController();
+        controller = GetComponent<CharacterController>();
+	}
+	
+	public void OnEnable()
+	{
+		PlayerInput.Enable();
+	}
+	
+	public void OnDisable()
+	{
+		PlayerInput.Disable();
+	}
+	
+	
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+		Vector2 movementInput = PlayerInput.PlayerMain.Move.ReadValue<Vector2>();
+        Vector3 move = new Vector3(movementInput.x, 0f, movementInput.y);
+        controller.Move(move * Time.deltaTime);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+        Debug.Log(gameObject.transform.position);
+        if (movementInput != Vector2.zero)
+        {
+            
+            gameObject.transform.forward = move;
+        }
+
+        
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
